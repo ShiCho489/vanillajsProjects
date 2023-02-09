4da799015e79f65c1568f3d83cbd961a6c52d1b2
Shivani Choudhary <choudhary.shivani489@gmail.com>
2023-02-09 09:47:07 +0200

    function declaration

--- calculationapp/index.js
+++ calculationapp/index.js
@@ -1,5 +1,5 @@
-let num1 = 8
-let num2 = 2
+let num1 = 10
+let num2 = 9
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
 
@@ -14,5 +14,20 @@ let sumEl = document.getElementById("sum-el")
 
 function add() {
     let result = num1 + num2
-    sumEl.textContent = "Sum: " + num1 + num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function subtract() {
+    let result = num1 - num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function divide() {
+    let result = num1 / num2
+    sumEl.textContent = "Sum: " + result
+}
+
+function multiply() {
+    let result = num1 * num2
+    sumEl.textContent = "Sum: " + result
 }
